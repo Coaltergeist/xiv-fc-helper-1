0223ad74d27154d1bb9e789a672f210a87a5ae95
Coalter <coaltergeist@gmail.com>
2018-06-07 17:23:41 -0400

    attempted to parse html, failed lol

--- ffxivmb/listing.go
+++ ffxivmb/listing.go
@@ -1,7 +1,6 @@
 package ffxivmb
 
 import (
-	"errors"
 	"fmt"
 
 	"github.com/PuerkitoBio/goquery"
@@ -10,6 +9,7 @@ import (
 
 // GetMBData returns some shit lol
 func GetMBData(ffxivmbURL string) (*structs.MBListing, error) {
+	fmt.Println(ffxivmbURL)
 	req := NewMBRequest()
 	req.SetURL(ffxivmbURL)
 	body := req.queue().Consume()
@@ -20,29 +20,43 @@ func GetMBData(ffxivmbURL string) (*structs.MBListing, error) {
 	}
 
 	listing := &structs.MBListing{}
-	fmt.Println("ok")
 
-	currentPrices := doc.Find("table.table2")
-	//history := doc.Find("table.table3")
+	// currentPrices := doc.Find("table.table2")
+	// //history := doc.Find("table.table3")
+	// fmt.Println(currentPrices.Text())
 
-	currentPrices = currentPrices.Find("tbody")
-	//history = history.Find("tbody")
+	// currentPrices = currentPrices.Find("tbody")
+	// //history = history.Find("tbody")
+	// fmt.Println(currentPrices.Text())
 
-	lowestPrice := currentPrices.Find("tr.odd")
+	// lowestPrice := currentPrices.Find("tr.odd")
+	// fmt.Println(lowestPrice.Text())
 
-	for i := range lowestPrice.Nodes {
-		if lowestPrice.Nodes[i].Data == "No data available in table" {
-			return nil, errors.New("That item has no listing")
-		}
-		if i == 1 {
-			(listing.Sale).Price = lowestPrice.Nodes[i].Data
-			fmt.Println((listing.Sale).Price)
-		} else if i == 2 {
-			(listing.Sale).Quantity = lowestPrice.Nodes[i].Data
-			fmt.Println((listing.Sale).Quantity)
-		} else if i == 3 {
-			(listing.Sale).Total = lowestPrice.Nodes[i].Data
-			fmt.Println((listing.Sale).Total)
+	// for i := range lowestPrice.Nodes {
+	// 	if lowestPrice.Nodes[i].Data == "No data available in table" {
+	// 		return nil, errors.New("That item has no listing")
+	// 	}
+	// 	if i == 1 {
+	// 		(listing.Sale).Price = lowestPrice.Nodes[i].Data
+	// 		fmt.Println((listing.Sale).Price)
+	// 	} else if i == 2 {
+	// 		(listing.Sale).Quantity = lowestPrice.Nodes[i].Data
+	// 		fmt.Println((listing.Sale).Quantity)
+	// 	} else if i == 3 {
+	// 		(listing.Sale).Total = lowestPrice.Nodes[i].Data
+	// 		fmt.Println((listing.Sale).Total)
+	// 	}
+	// }
+
+	a := make([]string, 0)
+
+	doc.Find("td").Each(func(i int, s *goquery.Selection) {
+		a = append(a, s.Text())
+	})
+
+	for i := 0; i < 100; i++ {
+		if a[i] == "" {
+			fmt.Println("ok")
 		}
 	}
 
